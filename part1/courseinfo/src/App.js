1f8f4a5bc575465b7f9704532552d359dd151d7a
Phuc Le <phuclevinh2000@gmail.com>
2021-08-25 22:42:32 +0300

    1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = ({course}) => {
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+    )
+  }
+
+  const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {part1}: {exercises1}<br/>
+          {part2}: {exercises2}<br/>
+          {part3}: {exercises3}<br/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    )
+  }
+
+  const Total = ({exercises1, exercises2, exercises3}) => {
+    const sum = exercises1 + exercises2 + exercises3
+    return (
+      <div>
+        <p>Number of exercises: {sum}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+        part1 = {part1}
+        part2 = {part2}
+        part3 = {part3}
+        exercises1 = {exercises1}
+        exercises2 = {exercises2}
+        exercises3 = {exercises3}
+      />
+      <Total 
+        exercises1 = {exercises1}
+        exercises2 = {exercises2}
+        exercises3 = {exercises3}
+      />
+      {/* <p>Number of exercises  {exercises1 + exercises2 + exercises3}</p> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
