d43dcc2f1ee2e9146c7216a9283a9867124cb635
Phuc Le <phuclevinh2000@gmail.com>
2021-08-27 01:09:42 +0300

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,59 +1,73 @@
 import React, { useState } from 'react'
 
+const Header = ({head}) => {
+  return (
+    <h1>
+      {head}
+    </h1>
+  )
+}
+
+const Button = ({good, setGood, bad, setBad, neutral, setNeutral}) => {
+  return (
+    <div>
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button onClick={() => setBad(bad+1)}>bad</button>
+    </div>
+  )
+}
+
+const Result = ({good, bad, neutral}) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const Calculation = ({good, bad, neutral}) => {
+  const all = good + bad + neutral
+  const positive = good *100 / all
+  const average = (good - bad) / all
+  return (
+    <div>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const Header = ({head}) => {
-    return (
-      <h1>
-        {head}
-      </h1>
-    )
-  }
-
-  const Button = () => {
-    return (
-      <div>
-        <button onClick={() => setGood(good+1)}>good</button>
-        <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-        <button onClick={() => setBad(bad+1)}>bad</button>
-      </div>
-    )
-  }
-
-  const Result = () => {
-    return (
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-      </div>
-    )
-  }
-
-  const Calculation = () => {
-    const all = good + bad + neutral
-    const positive = good *100 / all
-    const average = (good - bad) / all
-    return (
-      <div>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive}%</p>
-      </div>
-    )
-  }
   
   return (
     <div>
       <Header head="give feedback"/>
-      <Button />
-      <Header head="give feedback"/>
-      <Result />
-      <Calculation />
+      <Button 
+        good={good} 
+        setGood={setGood}
+        bad={bad}
+        setBad={setBad}
+        neutral={neutral}
+        setNeutral={setNeutral}/>
+      <Header head="statistics"/>
+      <Result 
+        good={good} 
+        bad={bad}
+        neutral={neutral}
+      />
+      <Calculation 
+         good={good} 
+         bad={bad}
+         neutral={neutral}
+      />
     </div>
   )
 }
