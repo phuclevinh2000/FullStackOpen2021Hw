7ee11cdced2273638fc2209f06c72fd7865cf4d4
Phuc Le <phuclevinh2000@gmail.com>
2021-08-27 00:57:32 +0300

    1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const Header = ({head}) => {
+    return (
+      <h1>
+        {head}
+      </h1>
+    )
+  }
+
+  const Button = () => {
+    return (
+      <div>
+        <button onClick={() => setGood(good+1)}>good</button>
+        <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+        <button onClick={() => setBad(bad+1)}>bad</button>
+      </div>
+    )
+  }
+
+  const Result = () => {
+    return (
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header head="give feedback"/>
+      <Button />
+      <Header head="give feedback"/>
+      <Result />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
