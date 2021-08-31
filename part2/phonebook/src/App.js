8122db88e89602c555b59898ea480bc604afcce2
Phuc Le <phuclevinh2000@gmail.com>
2021-08-31 16:37:50 +0300

    2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Note from './components/Note'
+import Header from './components/Header'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' },  //hardcode some name
+    { name: 'Ada Lovelace'}
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    //create a new object by what you typed
+    const note = {
+      name: newName
+    }
+
+    //copy the new object to the end of the old object
+    setPersons(persons.concat(note))  
+    setNewName('')  //clear the input
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header head="Phonebook"/>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <Header head="Numbers"/>
+      {persons.map(person =>      //use map to fetch all the name
+        <Note key={person.name} name={person.name}/>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ part2/phonebook/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({head}) => {
+    return (
+        <h2>
+            {head}
+        </h2>
+    )
+}
+
+export default Header
